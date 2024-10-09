8a010c2240402dfe90e92b31380aa24a50c45664
Your Name <rishurajk59@gmail.com>
2024-10-09 19:30:57 +0530

    edit for test case

--- xcountrieswithsearch/src/Component/Card/Card.jsx
+++ xcountrieswithsearch/src/Component/Card/Card.jsx
@@ -5,34 +5,20 @@ import { DataContext } from "../../Page/Home";
 
 export const Card = () => {
   const { filteredCardData, isLoading } = useContext(DataContext);
-  console.log("inCard", filteredCardData);
   return (
     <>
-      {console.log(filteredCardData, "inCard")}
-      {!isLoading ? (
-        filteredCardData.length > 0 ? (
-          filteredCardData.map((item, index) => {
-            return (
-              <div className={Styles.countryCard} key={index}>
-                <img
-                  className={Styles.imges}
-                  src={item.flags.svg}
-                  alt={item.name.common} 
-                />
-                <h2>{item.name.common}</h2>
-              </div>
-            );
-          })
-        ) : (
-          <div className="loader">
-            <h2> Not Found</h2>
+      {filteredCardData.map((item, index) => {
+        return (
+          <div className={Styles.countryCard} key={index}>
+            <img
+              className={Styles.imges}
+              src={item.flags.svg}
+              alt={item.name.common}
+            />
+            <h2>{item.name.common}</h2>
           </div>
-        )
-      ) : (
-        <div className="loader">
-          <CircularProgress color="primary" />
-        </div>
-      )}
+        );
+      })}
     </>
   );
 };
