8b8f3731a13a46676ffd214666114e42d568e211
Your Name <rishurajk59@gmail.com>
2024-10-20 17:27:20 +0530

    add xcountrieswithsearch

--- xcountrieswithsearch/src/App.js
+++ xcountrieswithsearch/src/App.js
@@ -1,40 +1,51 @@
 import React, { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./Page/Home";
-import NotFound from "./Page/NotFound";
 import axios from "axios";
-import { createContext } from "react";
-export const DataContext = createContext();
 
-function App() {
-  const [cardData, setCardData] = useState([]);
-  const [filteredCardData, setFilteredCardData] = useState([]);
-
-  const fetchApi = async () => {
-    try {
-      const response = await axios.get(`https://restcountries.com/v3.1/all`);
-      setCardData(response.data);
-      setFilteredCardData(response.data);
-    } catch (error) {
-      console.error(`Error fetching data:${error}`);
-    }
-  };
-
-  // console.log("CardData", cardData);
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    fetchApi();
+    const fetchCountries = async () => {
+      try {
+        const response = await axios.get(`https://restcountries.com/v3.1/all`);
+        setCountries(response.data);
+      } catch (error) {
+        console.error("Error fetching countries:", error);
+      }
+    };
+    fetchCountries();
   }, []);
 
-  return (
-    <DataContext.Provider
-      value={{ cardData, setFilteredCardData, filteredCardData }}
-    >
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </DataContext.Provider>
+  // Filter countries based on search input
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
   );
-}
+
+  return (
+    <div className="App">
+     <nav className="navbar">
+     <input
+        type="text"
+        placeholder="Search for a country..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+     </nav>
+      <div className="countryGrid">
+        {filteredCountries.length > 0 ? (
+          filteredCountries.map((country) => (
+            <div className="countryCard" key={country.cca3}>
+              <img src={country.flags.png} alt={country.name.common} />
+              <h3>{country.name.common}</h3>
+            </div>
+          ))
+        ) : (
+          <p>No countries found.</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
 export default App;
