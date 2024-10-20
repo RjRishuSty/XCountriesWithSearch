f950f3ec6d6ebc354ccc3babf729117b491aabc9
Your Name <rishurajk59@gmail.com>
2024-10-20 22:00:57 +0530

    change tage h3 to h2

--- xcountrieswithsearch/src/App.js
+++ xcountrieswithsearch/src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
--- xcountrieswithsearch/src/Page/Home.jsx
+++ xcountrieswithsearch/src/Page/Home.jsx
@@ -16,7 +16,7 @@ const Home = ({ countries, searchHandle }) => {
           countries.map((country) => (
             <div className="countryCard" key={country.cca3}>
               <img src={country.flags.png} alt={country.name.common} />
-              <h3>{country.name.common}</h3>
+              <h2>{country.name.common}</h2>
             </div>
           ))
         ) : (
