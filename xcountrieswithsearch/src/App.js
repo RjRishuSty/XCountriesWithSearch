842e1e935b4971e10124cde426b8da224780af47
Your Name <rishurajk59@gmail.com>
2024-10-20 18:48:27 +0530

    modify app.js

--- xcountrieswithsearch/src/App.js
+++ xcountrieswithsearch/src/App.js
@@ -4,42 +4,55 @@ import axios from "axios";
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [filterCountries, setFilterCountries] = useState([]);
 
+  const fetchCountries = async () => {
+    try {
+      const response = await axios.get(`https://restcountries.com/v3.1/all`);
+      setCountries(response.data);
+      setFilterCountries(response.data); 
+    } catch (error) {
+      console.error("Failed to fetch the data ", error);
+    }
+  };
+
+  const searchHandle = (e) => {
+    setSearchTerm(e.target.value.toLowerCase());
+  };
+
+ 
   useEffect(() => {
-    const fetchCountries = async () => {
-      try {
-        const response = await axios.get(`https://restcountries.com/v3.1/all`);
-        setCountries(response.data);
-      } catch (error) {
-        console.error("Error fetching countries:", error);
-      }
-    };
+    const filtered = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(searchTerm)
+    );
+    setFilterCountries(filtered);
+  }, [searchTerm, countries]);
+
+ 
+  useEffect(() => {
     fetchCountries();
   }, []);
 
-  // Filter countries based on search input
-  const filteredCountries = countries.filter((country) =>
-    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
   return (
     <div className="App">
-     <nav className="navbar">
-     <input
-        type="text"
-        placeholder="Search for a country..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-     </nav>
+      <nav className="navbar">
+        <input
+          type="text"
+          placeholder="Search for countries..."
+          value={searchTerm}
+          onChange={searchHandle}
+        />
+      </nav>
       <div className="countryGrid">
-        {filteredCountries.length > 0 && (
-          filteredCountries.map((country) => (
+        {filterCountries.length > 0 ? (
+          filterCountries.map((country) => (
             <div className="countryCard" key={country.cca3}>
               <img src={country.flags.png} alt={country.name.common} />
               <h3>{country.name.common}</h3>
             </div>
           ))
+        ) : (
+         null
         )}
       </div>
     </div>
